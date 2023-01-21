a7f13b7c2680bd29f578bed62e30805aa32fc06f
Max <shadow.iqka@qq.com>
2023-01-21 15:50:13 +0800

    [add] initialization

---
+++ services/foo.js
@@ -0,0 +1,15 @@
+/**
+ * Cloud Function
+ *
+ * POST /api/__yao/app/service/foo
+ * {"method":"Bar", "args":["hello", "world"]}
+ *
+ * @param  {...any} args
+ * @returns
+ */
+function Bar(...args) {
+  return {
+    message: $L("Another yao application") + " (Cloud Function: foo.Bar)",
+    args: args,
+  };
+}
