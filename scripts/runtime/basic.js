0b13a969c62ab4dcaed502097cf1fbf9f344352d
Max <shadow.iqka@qq.com>
2023-09-11 21:38:08 +0800

    [add] tests

--- scripts/runtime/basic.js
+++ scripts/runtime/basic.js
@@ -6,3 +6,14 @@ function Cancel(...args) {
   time.Sleep(500);
   return args[0];
 }
+
+function Error(...args) {
+  callStackTest(...args);
+  return args[0];
+}
+
+function callStackTest(...args) {
+  var obj = {};
+  obj.a();
+  return args;
+}
